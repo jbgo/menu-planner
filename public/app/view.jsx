131f8d0aff9a8315699eb51fabc6fea45f4faa5e
Jordan Bach <jordanbach@gmail.com>
2015-03-07 09:32:03 -0600

    resolved that pesky binding issue

--- public/app/view.jsx
+++ public/app/view.jsx
@@ -2,21 +2,11 @@ MP.MenuView = Backbone.View.extend({
   el: '#menu-plan',
 
   initialize: function() {
-    // Backbone appears to be doing some funky shit that prevents _.bind and
-    // _.bindAll from working correctly, so I created my own implementation to
-    // enfore some sense of "this" sanity here.
-    var _this = this;
-    _.each(_.functions(this), function(funcName) {
-      var func = _this[funcName];
-      _this[funcName] = function() { return func.apply(_this, arguments); }
-    });
-
     this.meals = new MP.MealCollection();
-    this.meals.on('sync', this.render);
-    this.meals.on('change', this.render);
-    this.meals.on('remove', this.render);
+    this.meals.on('sync', this.render, this);
+    this.meals.on('change', this.render, this);
+    this.meals.on('remove', this.render, this);
     this.meals.fetch();
-
     this.render();
   },
 
